c1c7863b6b6b8328b167fd4b3d5395be2891bb41
Hack Reactor Student <student@hackreactor.com>
2017-07-10 16:30:18 -0700

    Completed all

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor () {
+    super();
+    this.job = 'gamble';
+    this.age = 40;
+    this.canFly = false;
+    this.color = 'grey';
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
